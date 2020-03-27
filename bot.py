fc8b61b52ae881fb862266f24eba0f2a1c3846dd
/ kurp / <kurpingspace@gmail.com>
2020-03-27 17:56:37 +0000

    updated a few things
    changed guilds into a dict
    changed a few error phrases
    @commands.is_owner() does not work, so i made my own
    added a line to create a server object when joining a new server.
    added a few notes

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ import re
 import emoji
 
 messages = ["God fucking damn it, {0}", "Fuck you, {0}", "Leave me alone, I swear to god. You're so fucking annoying and it pisses me off, {0}", "FUCK OFF! {0}", "Pleeease bother someone else oh my fucking god, {0}", "I hope you actually fucking die, {0}"]
-guilds = []
+guilds = {}
 
 def changesetting(ctx, line, contents):
     with open("{0}-settings.txt".format(ctx.guild.id)) as file:
@@ -25,19 +25,18 @@ class Server:
         self.whitelisted = w
 
 def setting(guildId):
-    for guild in guilds:
-        if guildId in guild:
-            return guild[guildId]
+    if guildId in guilds:
+        return guilds[guildId]
     return None
 
 async def determine_prefix(bot, msg):
-    if msg.guild:
+    if msg.guild: #if message is in a guild
         return setting(msg.guild.id).prefix
     else:
         return "-"
     
 bot = commands.Bot(command_prefix=determine_prefix)
-bot.remove_command('help')
+bot.remove_command('help') #removes the prebuilt help command
 
 @bot.check
 async def globally_block_dms(ctx):
@@ -46,8 +45,12 @@ async def globally_block_dms(ctx):
 async def admin(ctx):
     if ctx.author.top_role >= ctx.guild.get_role(setting(ctx.guild.id).adminRole):
         return True
-    else:
-        return False
+    return False
+
+async def owner(ctx):
+    if ctx.author.id == ctx.guild.owner.id:
+        return True
+    return False
 
 @bot.event
 async def on_ready():
@@ -57,7 +60,7 @@ async def on_ready():
         with open(guild) as settings:
             settings = settings.readlines()
         settings = [item.strip() for item in settings]
-        guilds.append({ int(guild.strip("-settings.txt")): Server(bool(int(settings[0])), settings[1], int(settings[2]), int(settings[3]), [int(setting) for setting in settings[4].strip("[]").split(", ")])})
+        guilds[int(guild.strip("-settings.txt"))] = Server(bool(int(settings[0])), settings[1], int(settings[2]), int(settings[3]), [int(setting) for setting in settings[4].strip("[]").split(", ")])
     print('Here we go again {0.user}'.format(bot))
 
 @bot.event
@@ -68,16 +71,17 @@ async def on_guild_join(guild):
         file.write("Hi!")
     with open("servers.txt", "a") as file:
         file.write("\n{0}".format(guild.id))
-    print("Joined new server, and created files!")
     await guild.system_channel.send("Hi, i'm {0}! Please use -adminset to set the admin role and -prefix to change my prefix! Additionally, you can mention me or use the prefix to start commands! Use {0} help or -help for more info.".format(bot.user.mention))
+    guilds[guild.id] = Server(True, "-", guild.default_role.id, 12, [guild.system_channel.id])
+    print("Joined new server, and created files!")
 
 @bot.event
 async def on_message(message):
     if message.author == bot.user:
         return
-    if message.content.startswith((":", ";", "~", "-", "+", "=", ".", ",", "!", "$", "&", "^", "?")):
-            await bot.process_commands(message)
-            return
+    if message.content.startswith((":", ";", "~", "-", "+", "=", ".", ",", "!", "$", "&", "^", "?")): #ignores commands for every bot in a server, this isn't just brute-force
+        await bot.process_commands(message)
+        return #don't want it to continue after running a command
     if message.channel.id in setting(message.guild.id).whitelisted:
         if random.randint(1,10) == 10:
             with open("{0}-think.txt".format(message.guild.id)) as file:
@@ -95,7 +99,7 @@ async def on_message(message):
                 elif "\n" in message.content:
                     message.content = message.content.replace("\n", " ")
                     
-                message.content = emoji.demojize(message.content.strip())
+                message.content = emoji.demojize(message.content.strip()) #can't write unicode characters for some reason, gotta convert
                 if message.content in phrases:
                     print(message.content, " is already in phrases!")
                     return
@@ -114,12 +118,12 @@ async def on_message(message):
         if setting(message.guild.id).talk and random.randint(1, setting(message.guild.id).freq) == setting(message.guild.id).freq:
             with open("{0}-think.txt".format(message.guild.id)) as phraselist:
                 phrases = [line for line in phraselist.readlines()]
-            await message.channel.send(emoji.emojize(random.choice(phrases)))
+            await message.channel.send(emoji.emojize(random.choice(phrases))) #turns any emoji back into a unicode character and sends the message
 
 @bot.event
 async def on_command_error(ctx, err):
     if isinstance(err, commands.CheckFailure):
-        await ctx.send("Access Denied") 
+        await ctx.send("Invalid permissions!") 
     elif isinstance(err, commands.CommandNotFound):
         await ctx.send("Command not found! Try `{0}help` for a list of proper commands.".format(setting(ctx.guild.id).prefix))
 
@@ -170,11 +174,10 @@ async def prefix(ctx, newprefix):
         await ctx.send("Invalid prefix! Avaliable prefixes: `:;~-+=.,!$&^?`")
 
 @bot.command()
-@commands.is_owner()
+@commands.check(owner)
 async def adminset(ctx, role):
-    role = role.strip("<@&>")
     try:
-        role = int(role)
+        role = int(role.strip("<@&>"))
     except ValueError:
         await ctx.send("Please give a valid role!")
         return
@@ -206,16 +209,24 @@ async def freq(ctx, freq):
 @bot.command()
 @commands.check(admin) 
 async def talkchannel(ctx, *args):
-    flag = args[0]
+    try:
+        flag = args[0]
+    except IndexError:
+        await ctx.send("Please insert a flag after command! Flags are: a, rm, ?")
+        return
     if flag == "?":
         await ctx.send("Whitelisted channels: "+str(" ".join([ctx.guild.get_channel(textchannel).mention for textchannel in setting(ctx.guild.id).whitelisted])))
         return
-    channel = args[1].strip("<#>")
+    try:
+        channel = args[1].strip("<#>")
+        channel = int(channel)
+    except IndexError:
+        await ctx.send("Please insert a channel after flag!")
+        return
+    except ValueError:
+        await ctx.send("Please mention a channel or give channel ID!")
+        return
     if flag == "a":
-        try:
-            channel = int(channel)
-        except ValueError:
-            await ctx.send("Please give a valid channel!")
         if ctx.guild.get_channel(channel) == None:
             await ctx.send("This channel does not exist!")
         elif channel in setting(ctx.guild.id).whitelisted:
@@ -226,11 +237,6 @@ async def talkchannel(ctx, *args):
             await ctx.send("Added channel to whitelist!")
     elif flag == "rm":
         try:
-            channel = int(channel)
-        except ValueError:
-            await ctx.send("Please give a valid channel!")
-            return
-        try:
             setting(ctx.guild.id).whitelisted.remove(channel)
         except ValueError:
             await ctx.send("Could not find that in the whitelist!")
@@ -238,6 +244,6 @@ async def talkchannel(ctx, *args):
         changesetting(ctx, 4, setting(ctx.guild.id).whitelisted)
         await ctx.send("Channel removed!")
     else:
-        await ctx.send("Unknown flag!")
+        await ctx.send("Unknown flag! Flags are: a, rm, ?")
 
-bot.run() #insert the bot token there as str
+bot.run("") #insert the bot token there
